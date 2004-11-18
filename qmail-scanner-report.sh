693d124b1147a2d7b0da71a36287e828faf524ba
Elan Ruusamäe <glen@pld-linux.org>
2004-11-18 23:12:17 +0000

    - add patches / configs
    Changed files:
        qmail-scanner-attach.patch -> 1.1
        qmail-scanner-extsub.patch -> 1.1
        qmail-scanner-localconf-vars.patch -> 1.1
        qmail-scanner-localconf.patch -> 1.1
        qmail-scanner-perm.patch -> 1.1
        qmail-scanner-report.sh -> 1.1
        qmail-scanner-root.patch -> 1.1
        qmail-scanner.conf -> 1.1

---
+++ qmail-scanner-report.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+dir=`dirname "$0"`
+cd "$dir"
+
+day=${1:-yesterday}
+mask=`LANG=C date -d $day '+%a, %d %b %Y '`
+
+header="Virus report for $(LANG=C date -d $day '+%Y/%m/%d')"
+
+if tty -s; then
+	echo "$header"
+else
+	tmp=`mktemp ${TMPDIR:-/tmp}/qrepXXXXXX`
+	mail=1
+	exec 1>$tmp
+fi
+
+out=$(awk -F'\t' "/^$mask/ {print \$5} " < quarantine.log | sort | uniq -c | sort -nr)
+total=`echo "$out" | awk '{a+=$1}END{print a}'`
+
+cat <<EOF
+
+$(printf "%7d Total" $total)
+---------------------------
+$out
+EOF
+
+# 1079  awk -F'\t' '{print $5}'< quarantine.log |sort |uniq -c|sort -nr
+# 1083  awk -F'\t' '{if (!a) a=$1; b=$1} END{print "Start:", a, "\nEnd:", b}' < quarantine.log 
+
+if [ "$mail" ]; then
+	install -m644 $tmp reports/$(LANG=C date -d $day '+%Y%m%d').txt
+	mail -s "$(hostname -s): $header" ${MAILTO:-$LOGNAME} < $tmp
+	rm $tmp
+fi
